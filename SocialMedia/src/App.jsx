0dffdd4ae091f293388d0668dec8a798b7f74e92
Varshamishra56 <vm0222912@gmail.com>
2024-07-19 23:04:42 +0530

    deployment errors managed

--- SocialMedia/src/App.jsx
+++ SocialMedia/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import SideBar from "./Components/SideBar";
